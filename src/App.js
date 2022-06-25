21b931e6547427982c37ee18ddb533cc7cfac076
reo127 <64676438+reo127@users.noreply.github.com>
2022-06-25 17:14:57 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1> <span className="sub">Subcribe</span><br /> <span className='code'> CodeWithGeek </span> </h1>
+      
+    </div>
+  );
+}
+
+export default App;
+
+
+
+{/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
